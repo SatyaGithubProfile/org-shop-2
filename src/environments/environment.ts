33bd7d2fe4d6c72cf579ef831a496553b8259463
satya <teja151539@gmail.com>
2023-12-18 12:56:48 +0530

    * Setup the project  * Firebase setup  * Bootstrap installation

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase : {
+        apiKey: "AIzaSyBe26crceqmyGy3Rk8Ft5P--PTHpo6QiA4",
+        authDomain: "org-shop-2-ee3cd.firebaseapp.com",
+        projectId: "org-shop-2-ee3cd",
+        storageBucket: "org-shop-2-ee3cd.appspot.com",
+        messagingSenderId: "378623854483",
+        appId: "1:378623854483:web:03c7a48a09a9e065658a66",
+        measurementId: "G-NRS6NPBD9Z"
+    }
+};
